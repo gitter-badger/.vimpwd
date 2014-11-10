094b56f681fcec586f8158730e485a477f690ad5
Aubrey Portwood <aubreypwd@gmail.com>
2014-11-09 22:17:22 -0700

    Highlight spaces and tabs.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 ln -sf ~/.vimpwd/.vimrc.after ~/.vimrc.after
 ln -sf ~/.vimpwd/.vimrc.before ~/.vimrc.before
 ln -sf ~/.vimpwd/.janus ~/.janus
+ln -sf ~/.vimpwd/.gvimrc.after ~/.gvimrc.after
+ln -sf ~/.vimpwd/.gvimrc.before ~/.gvimrc.before
 git submodule init
 git submodule update
